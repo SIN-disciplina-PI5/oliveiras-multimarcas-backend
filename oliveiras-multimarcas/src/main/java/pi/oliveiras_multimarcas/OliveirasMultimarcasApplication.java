fa2b4f0548094d674eafc9e851ae8fc294740a34
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-11 21:09:18 -0300

    added: project base structure

---
+++ oliveiras-multimarcas/src/main/java/pi/oliveiras_multimarcas/OliveirasMultimarcasApplication.java
@@ -0,0 +1,13 @@
+package pi.oliveiras_multimarcas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveirasMultimarcasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveirasMultimarcasApplication.class, args);
+	}
+
+}
