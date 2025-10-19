e61e00d078f4fddad75ede085d847c09af170f7a
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-19 20:35:27 -0300

    added: seed in database

---
+++ src/main/java/pi/oliveiras_multimarcas/seed/DatabaseSeeder.java
@@ -0,0 +1,78 @@
+package pi.oliveiras_multimarcas.seed;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import pi.oliveiras_multimarcas.DTO.VehicleRequestDTO;
+import pi.oliveiras_multimarcas.models.ImageVehicle;
+import pi.oliveiras_multimarcas.models.Vehicle;
+import pi.oliveiras_multimarcas.services.VehicleService;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Profile("dev")
+public class DatabaseSeeder implements CommandLineRunner {
+
+    @Autowired
+    VehicleService vehicleService;
+
+    @Override
+    public void run(String... args){
+
+        VehicleRequestDTO vehicle1 = new VehicleRequestDTO();
+        vehicle1.setModel("Civic EXL");
+        vehicle1.setModelYear(2020);
+        vehicle1.setPrice(new BigDecimal("125000.00"));
+        List<String> images1 = new ArrayList<String>();
+
+        images1.add("https://example1/1");
+        images1.add("https://example1/2");
+        images1.add("https://example1/3");
+
+        vehicle1.setUrl_images(images1);
+        vehicle1.setDescription("Sedan médio com ótimo desempenho, conforto e baixo consumo de combustível.");
+        vehicle1.setMileage(32000);
+        vehicle1.setMark("Honda");
+
+        vehicleService.insert(vehicle1);
+
+        VehicleRequestDTO vehicle2 = new VehicleRequestDTO();
+        vehicle2.setModel("Corolla Altis Hybrid");
+        vehicle2.setModelYear(2016);
+        vehicle2.setPrice(new BigDecimal("145000.00"));
+        List<String> images2 = new ArrayList<String>();
+
+        images2.add("https://example2/1");
+        images2.add("https://example2/2");
+        images2.add("https://example2/3");
+
+        vehicle2.setUrl_images(images2);
+        vehicle2.setDescription("Versão híbrida do Corolla, unindo eficiência energética e conforto premium.");
+        vehicle2.setMileage(21000);
+        vehicle2.setMark("Toyota");
+
+        vehicleService.insert(vehicle2);
+
+        VehicleRequestDTO vehicle3 = new VehicleRequestDTO();
+        vehicle3.setModel("Compass Longitude");
+        vehicle3.setModelYear(2020);
+        vehicle3.setPrice(new BigDecimal("138000.00"));
+        List<String> images3 = new ArrayList<String>();
+
+        images3.add("https://example3/1");
+        images3.add("https://example3/2");
+        images3.add("https://example3/3");
+
+        vehicle3.setUrl_images(images3);
+        vehicle3.setDescription("SUV robusto com tração 4x4 e interior sofisticado, ideal para viagens longas.");
+        vehicle3.setMileage(12000);
+        vehicle3.setMark("Jeep");
+
+        vehicleService.insert(vehicle3);
+    }
+
+}
