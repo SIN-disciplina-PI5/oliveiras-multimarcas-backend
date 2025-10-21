effa8b5da169165a95b8a6939b7d57b300049375
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-21 03:24:47 -0300

    added: Model, repositorie and transactions in DB

--- src/main/java/pi/oliveiras_multimarcas/services/TokenService.java
+++ src/main/java/pi/oliveiras_multimarcas/services/TokenService.java
@@ -1,4 +1,39 @@
 package pi.oliveiras_multimarcas.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import pi.oliveiras_multimarcas.exceptions.NoSuchException;
+import pi.oliveiras_multimarcas.models.Token;
+import pi.oliveiras_multimarcas.repositories.TokenRepositorie;
+
+import java.util.Optional;
+
 public class TokenService {
+
+    @Autowired
+    TokenRepositorie tokenRepositorie;
+
+    @Transactional(readOnly = true)
+    public boolean isTokenActive(String token){
+        Optional<Token> tokenFinded = tokenRepositorie.findByToken(token);
+        return tokenFinded.isPresent();
+    }
+
+    @Transactional
+    public Token insert(String token){
+        Token newToken = new Token();
+        newToken.setToken(token);
+
+        newToken = tokenRepositorie.save(newToken);
+
+        return newToken;
+    }
+
+    @Transactional
+    public void deleteByToken(String token){
+        Optional<Token> tokenFinded = tokenRepositorie.findByToken(token);
+        if (tokenFinded.isEmpty()) throw new NoSuchException("Token");
+
+        tokenRepositorie.deleteByToken(token);
+    }
 }
