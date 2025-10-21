82f8380bf43a97c256cf5b9fef6aef40d25bd9d9
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-21 00:01:48 -0300

    added: passwordEncoder

--- src/main/java/pi/oliveiras_multimarcas/security/SecurityConfig.java
+++ src/main/java/pi/oliveiras_multimarcas/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,15 +28,17 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
-                        .requestMatchers(
-                                "/h2-console/**",
-                                "/swagger-ui.html",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.POST, "/vehicles**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/vehicles**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/vehicles**").authenticated()
+                        .anyRequest().permitAll()
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 }
