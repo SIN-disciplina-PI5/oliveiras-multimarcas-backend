704fee2bc93c359d3fa19ac5b9cf91d851f2fe03
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-21 03:19:00 -0300

    added: find by email function

--- src/main/java/pi/oliveiras_multimarcas/repositories/UserRepositorie.java
+++ src/main/java/pi/oliveiras_multimarcas/repositories/UserRepositorie.java
@@ -3,7 +3,9 @@ package pi.oliveiras_multimarcas.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pi.oliveiras_multimarcas.models.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepositorie extends JpaRepository<User, UUID> {
+    public Optional<User> findByEmail(String email);
 }
