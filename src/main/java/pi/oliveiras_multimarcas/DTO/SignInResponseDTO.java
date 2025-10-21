6a71fbd81e52dd0cd51efddff099c18b3e639fef
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-21 03:31:03 -0300

    added: response dto

--- src/main/java/pi/oliveiras_multimarcas/DTO/SignInResponseDTO.java
+++ src/main/java/pi/oliveiras_multimarcas/DTO/SignInResponseDTO.java
@@ -1,4 +1,15 @@
 package pi.oliveiras_multimarcas.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class SignInResponseDTO {
+    private String refreshToken;
+    private String acessToken;
 }
