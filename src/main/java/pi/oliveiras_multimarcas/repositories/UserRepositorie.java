cd50d8c6e7fa9e304d35b0148b5fe564f3f88f83
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-20 23:52:23 -0300

    added: repositorie

--- src/main/java/pi/oliveiras_multimarcas/repositories/UserRepositorie.java
+++ src/main/java/pi/oliveiras_multimarcas/repositories/UserRepositorie.java
@@ -1,4 +1,9 @@
 package pi.oliveiras_multimarcas.repositories;
 
-public interface UserRepositorie {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pi.oliveiras_multimarcas.models.User;
+
+import java.util.UUID;
+
+public interface UserRepositorie extends JpaRepository<User, UUID> {
 }
