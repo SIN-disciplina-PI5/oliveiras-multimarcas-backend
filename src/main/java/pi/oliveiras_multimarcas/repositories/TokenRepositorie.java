920f47ce5e8abd857c654c7c755cf66de96c790b
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-10-24 01:38:40 -0300

    modified: added notation

--- src/main/java/pi/oliveiras_multimarcas/repositories/TokenRepositorie.java
+++ src/main/java/pi/oliveiras_multimarcas/repositories/TokenRepositorie.java
@@ -1,11 +1,13 @@
 package pi.oliveiras_multimarcas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pi.oliveiras_multimarcas.models.Token;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface TokenRepositorie extends JpaRepository<Token, UUID> {
     public Optional<Token> findByToken(String token);
     public void deleteByToken(String token);
--- src/main/java/pi/oliveiras_multimarcas/repositories/UserRepositorie.java
+++ src/main/java/pi/oliveiras_multimarcas/repositories/UserRepositorie.java
@@ -1,11 +1,13 @@
 package pi.oliveiras_multimarcas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pi.oliveiras_multimarcas.models.User;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepositorie extends JpaRepository<User, UUID> {
     public Optional<User> findByEmail(String email);
 }
