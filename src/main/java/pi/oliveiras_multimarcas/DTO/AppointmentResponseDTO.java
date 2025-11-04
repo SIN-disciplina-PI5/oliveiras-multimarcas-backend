db58bfa653bdbcd8bfae7e73404da83196c06e5c
Ewayrton Brito de Oliveira <ewayrtonbrito@gmail.com>
2025-11-04 13:47:31 -0300

    Create AppointmentResponseDTO

---
+++ src/main/java/pi/oliveiras_multimarcas/DTO/AppointmentResponseDTO.java
@@ -0,0 +1,38 @@
+package pi.oliveiras_multimarcas.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pi.oliveiras_multimarcas.models.Appointment;
+import pi.oliveiras_multimarcas.models.enums.Status;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AppointmentResponseDTO {
+    private UUID id;
+    private UUID vehicleId;
+    private String vehicleModel; // Denormalizado para conveniência
+    private UUID clientId;
+    private String clientName; // Denormalizado para conveniência
+    private LocalDate schedulingDate;
+    private LocalTime schedulingTime;
+    private String description;
+    private Status status;
+
+    public AppointmentResponseDTO(Appointment appointment) {
+        this.id = appointment.getId();
+        this.vehicleId = appointment.getVehicle().getId();
+        this.vehicleModel = appointment.getVehicle().getModel();
+        this.clientId = appointment.getClient().getId();
+        this.clientName = appointment.getClient().getUsername();
+        this.schedulingDate = appointment.getSchedulingDate();
+        this.schedulingTime = appointment.getSchedulingTime();
+        this.description = appointment.getDescription();
+        this.status = appointment.getStatus();
+    }
+}
