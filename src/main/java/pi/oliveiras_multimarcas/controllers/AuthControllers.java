8d95e63d3af357f0e52d1827e97b81166bd546f3
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-11-20 00:38:10 -0300

    fix: authentication layers

--- src/main/java/pi/oliveiras_multimarcas/controllers/AuthControllers.java
+++ src/main/java/pi/oliveiras_multimarcas/controllers/AuthControllers.java
@@ -1,97 +1,128 @@
 package pi.oliveiras_multimarcas.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import pi.oliveiras_multimarcas.DTO.SaleRequestDTO;
-import pi.oliveiras_multimarcas.DTO.SaleResponseDTO;
-import pi.oliveiras_multimarcas.exceptions.InvalidArguments;
+import pi.oliveiras_multimarcas.DTO.*;
 import pi.oliveiras_multimarcas.exceptions.NoSuchException;
-import pi.oliveiras_multimarcas.models.Sale;
-import pi.oliveiras_multimarcas.services.SaleService;
+import pi.oliveiras_multimarcas.models.Client;
+import pi.oliveiras_multimarcas.models.Employee;
+import pi.oliveiras_multimarcas.security.JwtUtil;
+import pi.oliveiras_multimarcas.services.ClientService;
+import pi.oliveiras_multimarcas.services.EmployeeService;
+import pi.oliveiras_multimarcas.services.TokenService;
 
-import java.util.List;
+import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/users")
-public class SaleController {
+@RequestMapping("/auth")
+public class AuthControllers {
 
     @Autowired
-    private SaleService saleService;
+    private EmployeeService employeeService;
 
-    @GetMapping
-    public ResponseEntity<List<SaleResponseDTO>> findAll() {
-        List<SaleResponseDTO> sales = saleService.findAll();
-        List<SaleResponseDTO> userResponseDTOS = saless.stream().map(user -> {
-            SaleResponseDTO userResponseDTO = new SaleResponseDTO();
-            userResponseDTO.setId(user.getId());
-            userResponseDTO.setName(user.getName());
-            userResponseDTO.setEmail(user.getEmail());
-            userResponseDTO.setPosition(user.getPosition());
-            return userResponseDTO;
-        }).collect(Collectors.toList());
-        return ResponseEntity.ok(userResponseDTOS);
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody EmployeeRequestDTO dto) {
+        try {
+            employeeService.insert(dto);
+        } catch (Exception e) {
+            ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<SaleResponseDTO> findById(@PathVariable UUID id) {
-        try {
-            SaleResponseDTO sale = saleService.findById(id);
-            SaleResponseDTO saleResponse = new SaleResponseDTO();
-            saleResponse.setId(sale.getId());
-            saleResponse.setName(sale.getName());
-            saleResponse.setEmail(sale.getEmail());
-            saleResponse.setPosition(sale.getPosition());
-            return ResponseEntity.ok().body(saleResponse);
+    @PostMapping("/signin")
+    public ResponseEntity<SignInResponseDTO> signin(@RequestBody SignInRequestDTO dto){
+
+        Employee employee;
+        Client client;
+        try{
+            employee = employeeService.findByEmail(dto.getEmail());
+            if (!passwordEncoder.matches(dto.getPassword(), employee.getPassword())) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+            String refreshToken = jwtUtil.generateTokenRefresh(employee.getId(), employee.getEmail());
+            String acessToken = jwtUtil.generateTokenAcess(employee.getId(), employee.getEmail());
+            tokenService.insert(refreshToken);
+            SignInResponseDTO signInResponseDTO = new SignInResponseDTO(refreshToken,acessToken);
+
+            return ResponseEntity.ok().body(signInResponseDTO);
         } catch (NoSuchException e) {
-            return ResponseEntity.status(404).build();
+            try{
+                client = clientService.findByEmail(dto.getEmail());
+                if (!passwordEncoder.matches(dto.getPassword(), client.getPassword())) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+                String refreshToken = jwtUtil.generateTokenRefresh(client.getId(), client.getEmail());
+                String acessToken = jwtUtil.generateTokenAcess(client.getId(), client.getEmail());
+                tokenService.insert(refreshToken);
+                SignInResponseDTO signInResponseDTO = new SignInResponseDTO(refreshToken,acessToken);
+
+                return ResponseEntity.ok().body(signInResponseDTO);
+            } catch (NoSuchException j) {
+                return ResponseEntity.notFound().build();
+            }
         }
     }
 
-    @PostMapping
-    public ResponseEntity<SaleResponseDTO> insert(@RequestBody SaleRequestDTO SaleRequestDTO) {
-        try {
-            if (SaleRequestDTO.getUsername() == null || SaleRequestDTO.getEmail() == null || SaleRequestDTO.getPassword() == null) {
-                return ResponseEntity.badRequest().build();
-            }
-            SaleResponseDTO user = saleService.insert(SaleRequestDTO);
-            SaleResponseDTO userResponseDTO = new SaleResponseDTO();
-            userResponseDTO.setId(user.getId());
-            userResponseDTO.setName(user.getName());
-            userResponseDTO.setEmail(user.getEmail());
-            userResponseDTO.setPosition(user.getPosition());
-            return ResponseEntity.status(201).body(userResponseDTO);
-        } catch (InvalidArguments e) {
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader HttpServletRequest authorization){
+        String authHeader = authorization.getHeader("Authorization");
+        String token = authHeader.substring(7);
+
+        try{
+            tokenService.deleteByToken(token);
+        } catch (NoSuchException e) {
             return ResponseEntity.badRequest().build();
         }
+
+        return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateById(@PathVariable UUID id, @RequestBody SaleRequestDTO saleRequestDTO) {
-        try {
-            SaleResponseDTO user = saleService.updateById(id, saleRequestDTO);
-            SaleResponseDTO userResponseDTO = new SaleResponseDTO();
-            userResponseDTO.setId(user.getId());
-            userResponseDTO.setName(user.getName());
-            userResponseDTO.setEmail(user.getEmail());
-            userResponseDTO.setPosition(user.getPosition());
-            return ResponseEntity.ok(userResponseDTO);
-        } catch (InvalidArguments e) {
-            return ResponseEntity.status(400).body(e.getMessage());
-        } catch (NoSuchException e) {
-            return ResponseEntity.status(404).body("Usuário não encontrado");
-        }
-    }
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshTokenResponseDTO> refreshToken(@RequestHeader HttpServletRequest authorization) {
+        String authHeader = authorization.getHeader("Authorization");
+        String token = authHeader.substring(7);
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable UUID id) {
-        try {
-            saleService.deleteById(id);
-            return ResponseEntity.ok().body("Usuário deletado");
-        } catch (NoSuchException e) {
-            return ResponseEntity.status(404).body("Usuário não encontrado");
+        boolean isTokenValid = jwtUtil.isTokenValid(token, "refresh");
+        if (!isTokenValid) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        boolean isTokenActive = tokenService.isTokenActive(token);
+        if (!isTokenActive) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        Map<String, Object> claim = jwtUtil.extractClaims(token, "refresh");
+        Object userId = claim.get("id");
+        UUID id = UUID.fromString(userId.toString());
+
+        Employee employee;
+        Client client;
+        try{
+            employee = employeeService.findById(id);
+            RefreshTokenResponseDTO acessToken = new RefreshTokenResponseDTO(jwtUtil.generateTokenAcess(id, employee.getEmail()));
+
+            return ResponseEntity.ok().body(acessToken);
+        } catch (NoSuchException e){
+            try{
+                client = clientService.findById(id);
+                RefreshTokenResponseDTO acessToken = new RefreshTokenResponseDTO(jwtUtil.generateTokenAcess(id, client.getEmail()));
+                return ResponseEntity.ok().body(acessToken);
+            }catch (NoSuchException j){
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
         }
     }
 }
--- src/main/java/pi/oliveiras_multimarcas/models/Token.java
+++ src/main/java/pi/oliveiras_multimarcas/models/Token.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -18,6 +19,16 @@ public class Token {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    @Column
+    @Column(nullable = false)
     private String token;
+    @Column(nullable = false)
+    private UUID userId;
+    @Column(nullable = false)
+    private LocalDateTime expires;
+
+    Token(String token, UUID userId, LocalDateTime expites){
+        this.token = token;
+        this.userId = userId;
+        this.expires = expites;
+    }
 }
