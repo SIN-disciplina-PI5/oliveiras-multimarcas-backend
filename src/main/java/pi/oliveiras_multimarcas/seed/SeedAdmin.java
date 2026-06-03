28319e97d4e1c9f200198c02d929eaecc70294c7
Deivyson Silva <deivyson.jd.silva@gmail.com>
2026-06-03 19:31:21 -0300

    modified change password

--- src/main/java/pi/oliveiras_multimarcas/seed/SeedAdmin.java
+++ src/main/java/pi/oliveiras_multimarcas/seed/SeedAdmin.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pi.oliveiras_multimarcas.dto.EmployeeRequestDTO;
+import pi.oliveiras_multimarcas.models.Employee;
 import pi.oliveiras_multimarcas.models.enums.UserRole;
 import pi.oliveiras_multimarcas.repositories.EmployeeRepository;
 import pi.oliveiras_multimarcas.services.EmployeeService;
@@ -21,25 +22,22 @@ public class SeedAdmin implements CommandLineRunner {
     @Override
     public void run(String... args){
 
-        if (employeeRepository.existsByEmail("admin@admin.com")){
-            EmployeeRequestDTO employee1 = new EmployeeRequestDTO();
-            employee1.setEmail("admin@admin.com");
-            employee1.setName("Ewayrton");
-            employee1.setPosition("Product Owner");
-            employee1.setContact("081900000001");
-            employee1.setCpf("12345678901");
-            employee1.setRole(UserRole.ADMIN);
+        EmployeeRequestDTO employee1 = new EmployeeRequestDTO();
+        employee1.setEmail("admin@admin.com");
+        employee1.setName("Ewayrton");
+        employee1.setPosition("Product Owner");
+        employee1.setContact("081900000001");
+        employee1.setCpf("12345678901");
+        employee1.setRole(UserRole.ADMIN);
 
-            try{
-                employeeService.insert(employee1);
-            } catch (RuntimeException e){
-                System.out.println("Error: "+ e.getMessage());
-            }
-
+        try{
+            employeeService.insert(employee1);
+        } catch (RuntimeException e){
+            System.out.println("Error: "+ e.getMessage());
         }
 
-
-
+        Employee employee = employeeService.findByEmail("admin@admin.com");
+        employeeService.changePassword(employee.getId(),"admin");
 
     }
 
