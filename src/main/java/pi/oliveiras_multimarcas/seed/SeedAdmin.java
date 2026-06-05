05e67b280dd688d8a7264f9832231e9d8efcc4bb
Deivyson Silva <deivyson.jd.silva@gmail.com>
2026-06-05 05:59:39 -0300

    added preferences in db to prod

--- src/main/java/pi/oliveiras_multimarcas/seed/SeedAdmin.java
+++ src/main/java/pi/oliveiras_multimarcas/seed/SeedAdmin.java
@@ -5,10 +5,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pi.oliveiras_multimarcas.dto.EmployeeRequestDTO;
+import pi.oliveiras_multimarcas.dto.PreferencesRequestDTO;
 import pi.oliveiras_multimarcas.models.Employee;
 import pi.oliveiras_multimarcas.models.enums.UserRole;
 import pi.oliveiras_multimarcas.repositories.EmployeeRepository;
 import pi.oliveiras_multimarcas.services.EmployeeService;
+import pi.oliveiras_multimarcas.services.PreferencesService;
 
 @Component
 @Profile("prod")
@@ -19,6 +21,9 @@ public class SeedAdmin implements CommandLineRunner {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private PreferencesService preferencesService;
     @Override
     public void run(String... args){
 
@@ -39,6 +44,20 @@ public class SeedAdmin implements CommandLineRunner {
         Employee employee = employeeService.findByEmail("admin@admin.com");
         employeeService.changePassword(employee.getId(),"admin");
 
+        PreferencesRequestDTO preferencesRequestDTO = getPreferencesRequestDTO();
+        preferencesService.updatePreferences(preferencesRequestDTO);
+
+    }
+
+    private static PreferencesRequestDTO getPreferencesRequestDTO() {
+        PreferencesRequestDTO preferencesRequestDTO = new PreferencesRequestDTO();
+        preferencesRequestDTO.setAddress("Rua Nossa Senhora de Fátima, 28 - Jardim São Paulo, Recife - PE");
+        preferencesRequestDTO.setUrlAddress("https://maps.app.goo.gl/cNPEU7RPPjkGY2697");
+        preferencesRequestDTO.setCnpj("62.332.520/0001-59");
+        preferencesRequestDTO.setContact("81994134298");
+        preferencesRequestDTO.setEmail("oliveirasmultimarcasof@gmail.com");
+        preferencesRequestDTO.setUrlInstagram("https://www.instagram.com/oliveiras_multimarcas_of/");
+        return preferencesRequestDTO;
     }
 
 }
